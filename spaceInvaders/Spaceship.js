640c826b22d815b5cdfb62e4a88cb1ca31061d34
N103Gremillion <nathan103grem@gmail.com>
2024-03-19 19:43:19 -0500

    yo

--- spaceInvaders/Spaceship.js
+++ spaceInvaders/Spaceship.js
@@ -1,14 +1,49 @@
 import Game_Object from "../Game_Object.js";
+import { InputMapping } from "../KeyboardMapping.js";
 
 export default class Spaceship extends Game_Object{
   
-  constructor(x, y, width, height, tag, spriteList){
+  constructor(x, y, width, height, tag, spriteList, velocity){
     super(x,y, tag);
     this.width = width;
     this.height = height;
     this.image = new Image();
     this.image.src = spriteList[0];
     this.spriteList = spriteList;
+    this.right = 'd';
+    this.left = 'a';
+    this.velocity = velocity;
+  }
+
+  render(ctx){
+    ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update(){
+    
+    if (InputMapping[this.right] === true && this.position.x < (window.innerWidth - this.width)){
+      this.position.x += this.velocity;
+    }
+    else if (InputMapping[this.left] === true && this.position.x > 0){
+      this.position.x -= this.velocity;
+    }
+  }
+
+  adjustValues(newCanvasWidth, newCanvasHeight, oldCanvasWidth, oldCanvasHeight) {
+    const scaleX = newCanvasWidth / oldCanvasWidth;
+    const scaleY = newCanvasHeight / oldCanvasHeight;
+    const oldHeight = this.height;
+
+    // Adjust positions
+    this.position.x *= scaleX;
+    this.position.y *= scaleY;
+
+    // Adjust the size while maintaining aspect ratio 
+    this.width *= scaleX;
+    this.height = (newCanvasHeight * oldHeight) / oldCanvasHeight;
+
+    // Adjust the velocity
+    this.velocity = window.innerHeight/200;
   }
 
 }
--- spaceInvaders/spaceInvadersMain.js
+++ spaceInvaders/spaceInvadersMain.js
@@ -7,13 +7,6 @@ import Spaceship from "./Spaceship.js";
 Manager.setCanvasById('SpaceInvadersCanvas', 'SinglePlayerMode', 'SpaceInvaders');
 
 // setup the game objects
-
-//set background
-const SPACEBACKGROUND = ['./sprites/spaceInvadersBackground.png'];
-const COLLISIONTAGS = []
-
-// setup the game and it's elements
-Manager.loadBackgroundImage(SPACEBACKGROUND, COLLISIONTAGS);
 Manager.setup((gameObjects, gameScores, Game_Manager) => {
   let CANVAS_WIDTH = Game_Manager.canvas.width;
   let CANVAS_HEIGHT = Game_Manager.canvas.height;
@@ -22,9 +15,17 @@ Manager.setup((gameObjects, gameScores, Game_Manager) => {
   let spaceshipWidth = CANVAS_WIDTH * 0.05;
   let spaceshipHeight = CANVAS_HEIGHT * 0.05;
   const SPACESHIPTAG = "Spaceship";
-  const SPCAESHIPSPRITES = ['','',''];
-  gameObjects.push(new Spaceship(spaceshipX, spaceshipY, spaceshipWidth, spaceshipHeight, SPACESHIPTAG, ));
+  const SPACESHIPSPRITES = ['./sprites/spaceShip1.png', './sprites/spaceShip2.png', './sprites/spaceShip3.png', './sprites/spaceShip4.png', './sprites/spaceShip5.png'];
+  const VELOCITY = CANVAS_WIDTH/200;
+  gameObjects.push(new Spaceship(spaceshipX, spaceshipY, spaceshipWidth, spaceshipHeight, SPACESHIPTAG, SPACESHIPSPRITES, VELOCITY));
 });
+
+//set background
+const SPACEBACKGROUND = ['./sprites/spaceInvadersBackground.png'];
+const COLLISIONTAGS = []
+
+// setup the game and it's elements
+Manager.loadBackgroundImage(SPACEBACKGROUND, COLLISIONTAGS);
 
 // start reading input from keyboardMapping constants
 Manager.inputHandling(InputMapping, Manager);
